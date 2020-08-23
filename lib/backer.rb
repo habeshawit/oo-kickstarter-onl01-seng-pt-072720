1f40e031cf97cf279dec54aa8dce02eabd1e6fab
Shalom Kebede <shalomseleshi@gmail.com>
2020-08-23 04:54:19 +0000

    Automatically backed up by Learn

---
+++ lib/backer.rb
@@ -0,0 +1,27 @@
+class Backer
+  attr_reader :name
+  attr_accessor :backed_projects,
+  
+  def initialize(name)
+    @name = name
+   # @backed_projects = []
+    @backed_projects = []
+  end
+  
+  def back_project(project)
+    backed_projects << project
+    #project.add_backer(self)
+    
+    # unless @backed_projects.each{|p| p == project}
+    #   backed_projects << project
+    #   project.add_backer(self)
+    # end 
+    # unless @backed_projects.include?(project)
+    #   #project.add_backer(self)
+    #   @backed_projects << project
+    # end
+
+    
+  end
+   
+end
